8f5cea03237f90650ccd2a49f0cf41721216ce8b
Ian Kersz <kerszamaral@gmail.com>
2022-03-08 12:59:01 -0300

    Primeiro programa de blink

---
+++ Aula_1/Programa_1.ino
@@ -0,0 +1,15 @@
+// Ian Kersz - Cartão ufrgs: 00338368
+// 00.03.22
+
+void setup()
+{
+    pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);
+    delay(1000);
+}
