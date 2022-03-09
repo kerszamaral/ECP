70359d7bbcadfeb1dc73ad14b2644b9589d7e000
Ian Kersz <kerszamaral@gmail.com>
2022-03-08 21:10:51 -0300

    Programa da Tarefa 2

--- Aula_1/Programa_3.ino
+++ Aula_1/Programa_3.ino
@@ -1,28 +1,68 @@
-// Ian Kersz - Cartão ufrgs: 00338368
-// 08.03.22
+//Ian Kersz - Cartão UFRGS: 00338368
+//08.03.22
 //Botão
 
-int ledPin = 13;
-int buttonPin = 8;
+#define TAM 3
+#define TEM 1000
+
+int led[TAM+1] = {12,11,10,9};
+int buttonPin = 2;
+
+void PISCAR()
+{
+    for (int i = TAM; i >= 0; i--)
+    {
+        digitalWrite(led[i], HIGH);
+        if (digitalRead(buttonPin) == 1)
+            return;
+        delay(TEM / 2);
+    }
+    for (int i = 0; i <= TAM; i++)
+    {
+        digitalWrite(led[i], LOW);
+        if (digitalRead(buttonPin) == 1)
+            return;
+        delay(TEM / 2);
+    }
+}
+
+void CONTADOR()
+{
+    int arr[4] = {0};
+    for (int i = 0; i < 16; i++)
+    {
+        arr[3] = i % 2;
+        arr[2] = i / 2 % 2;
+        arr[1] = i / 2 / 2 % 2;
+        arr[0] = i / 2 / 2 / 2 % 2;
+        for (int j = 0; j <= TAM; j++)
+        {
+            if (arr[j] == 1)
+            {
+                digitalWrite(led[j], HIGH);
+            }
+            else
+            {
+                digitalWrite(led[j], LOW);
+            }
+        }
+        delay(TEM);
+    }
+}
 
 void setup()
 {
-    pinMode(ledPin,OUTPUT);
-    pinMode(buttonPin,INPUT);
+    for (int i = 0; i <= TAM; i++)
+    {
+        pinMode(led[i], OUTPUT);
+    }
+    pinMode(buttonPin, INPUT);
 }
 
 void loop()
 {
-    int x;
-    x = digitalRead(buttonPin);
-
-    if (x == 1)
-    {
-        digitalWrite(ledPin,HIGH);
-    }
-    else if(x == 0)
-    {
-        digitalWrite(ledPin,LOW);
-    }
-    delay(1000);
+    if (digitalRead(buttonPin) == 1)
+        CONTADOR();
+    if(digitalRead(buttonPin) == 0)
+        PISCAR();
 }
