fa5edba10c666bf12f660fcb4b8734a5507668cf
Ian Kersz <kerszamaral@gmail.com>
2022-03-08 23:03:28 -0300

    Programa escalavel para N numero de LEDs

--- Aula_1/Programa_3.ino
+++ Aula_1/Programa_3.ino
@@ -1,40 +1,66 @@
 //Ian Kersz - Cartão UFRGS: 00338368
 //08.03.22
-//Botão
+//4 lEDs acendem em ordem crescente e apagam em ordem decrescente,
+//Ao pressionar o botão, os 4 lEDs viram um contador de 4 bits(ou seja, 16 numeros)
+//Após isso, retornam a sua função normal 
 
+//Definição da quantidade de lEDs(-1) e definição do tempo de delay em milisegundos
+//É possivel fazer o contador ter quantos bits quiser, 
+//basta incrementar o TAM(Será sempre o numero de LEDs -1) e adicionar a porta ao array
 #define TAM 3
 #define TEM 1000
 
+//Definição das variaveis globais, que são as portas dos LEDs e do botão
 int led[TAM+1] = {12,11,10,9};
 int buttonPin = 2;
 
-void PISCAR()
+//Criação da função para apagar todos os LEDs
+void APAGAR()
 {
     for (int i = TAM; i >= 0; i--)
     {
+        digitalWrite(led[i], LOW);
+        
+    }
+}
+//Criação da função para fazer os LEDs piscarem
+void PISCAR()
+{
+    //Ordem crescente
+    for (int i = TAM; i >= 0; i--)
+    {
         digitalWrite(led[i], HIGH);
+        //caso o botão seja pressionado no meio da função
         if (digitalRead(buttonPin) == 1)
             return;
+        //Delay mais curto
         delay(TEM / 2);
     }
+    //Ordem decrescente
     for (int i = 0; i <= TAM; i++)
     {
         digitalWrite(led[i], LOW);
+        //caso o botão seja pressionado no meio da função
         if (digitalRead(buttonPin) == 1)
             return;
+        //Delay mais curto
         delay(TEM / 2);
     }
 }
-
+//Criação da função para fazer os LEDs serem um contador de 4 bits
 void CONTADOR()
 {
-    int arr[4] = {0};
-    for (int i = 0; i < 16; i++)
+    //Definição do array para contar os bits
+    int arr[TAM+1] = {0};
+    //Loop para contar até 16
+    for (int i = 0; i < pow(2,TAM+1); i++)
     {
-        arr[3] = i % 2;
-        arr[2] = i / 2 % 2;
-        arr[1] = i / 2 / 2 % 2;
-        arr[0] = i / 2 / 2 / 2 % 2;
+        //Matematica para configurar qual LED está aceso no array ou não naquele momento
+        for (int j = TAM; j >= 0; j--)
+        {
+            arr[j]= i / (int)ceil(pow(2,TAM-j)) % 2;
+        }
+        //Loop para acender ou apagar o LED da posição j em função do bit localizado em j
         for (int j = 0; j <= TAM; j++)
         {
             if (arr[j] == 1)
@@ -48,21 +74,29 @@ void CONTADOR()
         }
         delay(TEM);
     }
+    APAGAR();
 }
-
+//Setup das portas do arduino
 void setup()
 {
+    //Loop para configurar todos os LEDs e depois o botão
     for (int i = 0; i <= TAM; i++)
     {
         pinMode(led[i], OUTPUT);
     }
     pinMode(buttonPin, INPUT);
 }
-
+//Loop de execução do arduino
 void loop()
 {
-    if (digitalRead(buttonPin) == 1)
+    //Função para ver se o botão está pressionado ou não
+    switch (digitalRead(buttonPin))
+    {
+    case 1: //Caso sim, inicia o contador
         CONTADOR();
-    if(digitalRead(buttonPin) == 0)
+        break;
+    case 0: //caso não, inicia piscar
         PISCAR();
+        break;
+    }
 }
